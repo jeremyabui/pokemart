e32793624504d4c7baaf4d6e5046a266615e9004
jbui <jeremyabui@yahoo.com>
2019-12-08 10:01:53 -0800

    Created link to product details

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 class Product extends React.Component {
   render() {
     return (
       <>
         <div className='product-container'>
-          <img src={this.props.productData.image} alt={this.props.productData.name}/>
+          <Link to={'/mall/'+ this.props.productData._id} >
+            <img src={this.props.productData.image} alt={this.props.productData.name}/>
+          </Link>
           <h4>{this.props.productData.name}</h4>
           <p>{this.props.productData.description}</p>
           <p>${this.props.productData.price}</p>
