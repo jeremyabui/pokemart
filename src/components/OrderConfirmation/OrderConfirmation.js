fac26fe04b38bf3b479a979f8078dd6cc5b6e91f
jbui <jeremyabui@yahoo.com>
2019-12-11 08:34:15 -0800

    More styling

--- src/components/OrderConfirmation/OrderConfirmation.js
+++ src/components/OrderConfirmation/OrderConfirmation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const OrderConfirmation = (props) => {
   return (
-    <div>
+    <div className='orderConfirmationContainer'>
       <h2>Thank you for your purchase.</h2>
       <p>You will receive an invoice for your order shortly.</p>
     </div>
